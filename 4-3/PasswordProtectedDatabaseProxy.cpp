b7c9727aa73310394b5e72e2984bccb440395cc6
黃千祐 <jearlrunner@gmail.com>
2024-04-20 13:28:05 +0800

    加上exit和cin.get

--- 4-3/PasswordProtectedDatabaseProxy.cpp
+++ 4-3/PasswordProtectedDatabaseProxy.cpp
@@ -18,14 +18,20 @@ shared_ptr<Employee> PasswordProtectedDatabaseProxy::GetEmployeeById(int id)
 	errno_t err = _dupenv_s(&varValue, &size, varName);
 	if (err) {
 		cout << "Error code : " << err;
+		cin.get();
+		exit(0);
 	}
 
 	if (varValue == nullptr) {
 		cout << "Environment variable " << varName << " is not set.\n";
+		cin.get();
+		exit(0);
 	}
 	else {
 		if (strcmp(varValue, "1qaz2wsx") != 0) {
 			cout << "Password error!!\n";
+			cin.get();
+			exit(0);
 		}
 		else {
 			employee = VirtualDatabaseProxy::GetEmployeeById(id);
